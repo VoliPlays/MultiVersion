64194f90edb9a381af060cdacae372c5a3e42450
debil9877654321 <33634290+debil9877654321@users.noreply.github.com>
2017-12-12 20:16:17 +0100

    Version check

--- src/MultiVersion/Loader.php
+++ src/MultiVersion/Loader.php
@@ -1,1 +1,24 @@
+<?php
 
+namespace MultiVersion;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
+
+class Loader extends PluginBase{
+  
+  public $oldClients = [];
+  
+  public function onEnable(){
+    if(ProtocolInfo::CURRENT_PROTOCOL !== 150){
+      $this->getLogger()->error('Server must accept protocol 150 as its default to run MultiVersion without problems.');
+      $this->setEnabled(false);
+    }
+  }
+  
+  public function onDisable(){
+    //TODO: handle DisconnectPacket for 1.1 clients
+  }
+  
+  
+}
