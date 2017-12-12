0088c719c7865e3f06b2561bc4afdfbd454b81cd
debil9877654321 <33634290+debil9877654321@users.noreply.github.com>
2017-12-12 20:22:16 +0100

    Update Loader.php

--- src/MultiVersion/Loader.php
+++ src/MultiVersion/Loader.php
@@ -7,13 +7,23 @@ use pocketmine\network\mcpe\protocol\ProtocolInfo;
 
 class Loader extends PluginBase{
   
+  /** @var array $oldClients */
   public $oldClients = [];
+  
+  /** @var bool $onlineMode */
+  public $onlineMode = false;
   
   public function onEnable(){
     if(ProtocolInfo::CURRENT_PROTOCOL !== 150){
       $this->getLogger()->error('Server must accept protocol 150 as its default to run MultiVersion without problems.');
       $this->setEnabled(false);
+      return false;
     }
+    $this->saveDefaultConfig();
+    
+    $this->onlineMode = $this->getConfig()->get('onlineMode');
+    $this->getServer()->registerEvents(new EventListener($this), $this);
+    
   }
   
   public function onDisable(){
