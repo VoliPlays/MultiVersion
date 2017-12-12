cfb0592608b72015cc490242f53508671264b01d
debil9877654321 <33634290+debil9877654321@users.noreply.github.com>
2017-12-12 20:24:31 +0100

    Update EventListener.php

--- src/MultiVersion/EventListener.php
+++ src/MultiVersion/EventListener.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace MultiVersion;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerCreationEvent;
+
+class EventListener implements Listener{
+  
+  private $plugin;
+  
+  public function __construct(Loader $plugin){
+    $this->plugin = $plugin;
+  }
+  
+  public function getPlugin() : Loader{
+    return $this->plugin;
+  }
+  
+  public function onPlayerCreation(PlayerCreationEvent $e){
+    //TODO: Set class to MultiPlayer
+  }
+}
