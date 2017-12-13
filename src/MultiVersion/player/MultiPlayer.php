3ab254801c55ec36e713b96131f69af30d82062c
debil9877654321 <33634290+debil9877654321@users.noreply.github.com>
2017-12-13 16:56:04 +0100

    Update MultiPlayer.php

--- src/MultiVersion/player/MultiPlayer.php
+++ src/MultiVersion/player/MultiPlayer.php
@@ -1,1 +1,29 @@
+<?php
 
+namespace MultiVersion\player;
+
+use pocketmine\network\SourceInterface;
+
+class MultiPlayer{
+  
+  public $inventory;
+  
+  private $disallowedPackets = [52,35,46,47,22]; //TODO: Add other incompatible packets.
+  
+  public function __construct(SourceInterface $interface, $clientID, $ip, $port){
+        //TODO: set inventory
+        parent::__construct($interface, $clientID, $ip, $port);
+  }
+  
+  public function getDissalowedPackets() : array{
+    return $this->disallowedPackets;
+  }
+  
+  
+  
+  public function getInventory(){
+    //TODO: Send correct inventory
+  }
+  
+  
+}
